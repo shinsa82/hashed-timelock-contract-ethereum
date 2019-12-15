37159829cb035692791584c64c3b5475c0a6ffc4
Shin Saito <shinsa@jp.ibm.com>
2019-12-16 03:31:11 +0900

    add original test cases

--- evaluations/js-wrapper.1.js
+++ evaluations/js-wrapper.1.js
@@ -12,7 +12,7 @@ const {
 } = require('../test/helper/utils');
 
 const { model } = require('./htlc-model')
-const { stateMachine, END } = require('./state-machine')
+const { END } = require('./state-machine')
 const oneFinney = web3.utils.toWei(web3.utils.toBN(1), 'finney')
 const debugGasUsed = (tx) => debug('gas used:', tx.receipt.gasUsed)
 
@@ -64,8 +64,8 @@ const cash_withdraw = async ({ machine, Seller, b2aSwapId, hashPair, Cash }) => 
             from: Seller,
         })
         debugGasUsed(withdrawTx)
-
-        machine.send('Cash.withdraw_end')
+        if (machine)
+            machine.send('Cash.withdraw_end')
     } catch (err) {
         machine.send('Cash.withdraw_err_expired')
     }
@@ -124,6 +124,92 @@ contract('HashedTimelock DvP with JS wrapper', accounts => {
     const Seller = accounts[1] // owner of AliceERC20 and wants swap for BobERC20
     const Buyer = accounts[2] // owner of BobERC20 and wants to swap for AliceERC20
     const HashedTimelock = artifacts.require('./HashedTimelock.sol')
+    const { stateMachine } = require('./state-machine')
+
+    it('normal scenario (both seller and buyer withdraw)', async () => {
+        const machine = stateMachine(model)
+
+        const Security = await HashedTimelock.new()
+        const Cash = await HashedTimelock.new()
+        const hashPair = newSecretHashPair()
+
+        const context = { hashPair, machine, Security, Cash, Seller, Buyer, secDelta: 2, cashDelta: 1 }
+
+        context.a2bSwapId = await sec_newContract(context)
+        context.b2aSwapId = await cash_newContract(context)
+        await cash_withdraw(context)
+        await sec_withdraw(context)
+        await end(context)
+    })
+
+    it('fail scenario (buyer fails to withdraw)', async () => {
+        const machine = stateMachine(model)
+
+        const Security = await HashedTimelock.new()
+        const Cash = await HashedTimelock.new()
+        const hashPair = newSecretHashPair()
+
+        const context = { hashPair, machine, Security, Cash, Seller, Buyer, secDelta: 2, cashDelta: 1 }
+
+        context.a2bSwapId = await sec_newContract(context)
+        context.b2aSwapId = await cash_newContract(context)
+        await cash_withdraw(context)
+        await delay(2000)
+        await sec_withdraw(context)
+        await end(context)
+    })
+
+    it('normal scenario (both seller and buyer refund)', async () => {
+        const machine = stateMachine(model)
+
+        const Security = await HashedTimelock.new()
+        const Cash = await HashedTimelock.new()
+        const hashPair = newSecretHashPair()
+
+        const context = { hashPair, machine, Security, Cash, Seller, Buyer, secDelta: 2, cashDelta: 1 }
+
+        context.a2bSwapId = await sec_newContract(context)
+        context.b2aSwapId = await cash_newContract(context)
+
+        await delay(3000)
+        await cash_withdraw(context)
+        await cash_refund(context)
+        await sec_refund(context)
+        await end(context)
+    })
+
+    it('normal scenario (seller refund retry)', async () => {
+        const machine = stateMachine(model)
+
+        const Security = await HashedTimelock.new()
+        const Cash = await HashedTimelock.new()
+        const hashPair = newSecretHashPair()
+
+        const context = { hashPair, machine, Security, Cash, Seller, Buyer, secDelta: 3, cashDelta: 1 }
+
+        context.a2bSwapId = await sec_newContract(context)
+        context.b2aSwapId = await cash_newContract(context)
+
+        await delay(1500)
+        await cash_withdraw(context)
+        await cash_refund(context)
+        await sec_refund(context)
+        await sec_refund(context) // retry
+        await delay(1500)
+        await sec_refund(context) // retry again
+        await end(context)
+    })
+})
+
+contract('HashedTimelock DvP original', accounts => {
+    const Seller = accounts[1] // owner of AliceERC20 and wants swap for BobERC20
+    const Buyer = accounts[2] // owner of BobERC20 and wants to swap for AliceERC20
+    const HashedTimelock = artifacts.require('./HashedTimelock.sol')
+    const stateMachine = () => {
+        return {
+            send: () => { }
+        }
+    }
 
     it('normal scenario (both seller and buyer withdraw)', async () => {
         const machine = stateMachine(model)
